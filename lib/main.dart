3e1f90ef3285fd757ed9e90d7cac4d6e1e884e4e
Manuela Garcia <mgh9517@gmail.com>
2020-07-15 18:15:53 -0500

    Update code so the notifications are not called from main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,151 +1,157 @@
 import 'dart:async';
+import 'dart:io' as Platform;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:notificationapp/notification_screen.dart';
+import 'package:notificationapp/second_screen.dart';
+import 'package:rxdart/subjects.dart';
 
-import 'second_screen.dart';
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
-void main() => runApp(new MaterialApp(home: new MyApp()));
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+    BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String> selectNotificationSubject =
+    BehaviorSubject<String>();
+
+NotificationAppLaunchDetails notificationAppLaunchDetails;
+
+class ReceivedNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+
+  ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+      onDidReceiveLocalNotification:
+          (int id, String title, String body, String payload) async {
+        didReceiveLocalNotificationSubject.add(ReceivedNotification(
+            id: id, title: title, body: body, payload: payload));
+      });
+  var initializationSettings = InitializationSettings(
+      initializationSettingsAndroid, initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    selectNotificationSubject.add(payload);
+  });
+
+  runApp(MaterialApp(home: new MyApp()));
+}
 
 class MyApp extends StatefulWidget {
+  static final navigatorKey = new GlobalKey<NavigatorState>();
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-  var initializationSettingsAndroid;
-  var initializationSettingsiOS;
-  var initializationSettings;
-
-  scheduleNotification1() async {
-    var scheduledNotificationDateTime =
-        DateTime.now().add(Duration(seconds: 10));
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        'channel_ID', 'channel name', 'channel description',
-        priority: Priority.High,
-        importance: Importance.Max,
-        ticker: 'test ticker');
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    var platformChannelSpecifics = new NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.schedule(
-        0,
-        'New Video is out',
-        'Flutter Local Notification',
-        scheduledNotificationDateTime,
-        platformChannelSpecifics,
-        payload: 'test payload');
-  }
-
-  scheduleNotification2() async {
-    var scheduledNotificationDateTime2 =
-        DateTime.now().add(Duration(seconds: 20));
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        'channel_ID', 'channel name', 'channel description',
-        priority: Priority.High,
-        importance: Importance.Max,
-        ticker: 'test ticker');
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    var platformChannelSpecifics = new NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.schedule(
-        1,
-        'Notification2',
-        'Flutter Local Notification2',
-        scheduledNotificationDateTime2,
-        platformChannelSpecifics,
-        payload: 'test payload');
-  }
-
-  showNotification() async {
-    var scheduledNotificationDateTime =
-        DateTime.now().add(Duration(seconds: 5));
-    var scheduledNotificationDateTime2 =
-        DateTime.now().add(Duration(seconds: 10));
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-        'channel_ID', 'channel name', 'channel description',
-        priority: Priority.High,
-        importance: Importance.Max,
-        ticker: 'test ticker');
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    var platformChannelSpecifics = new NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.schedule(
-        0,
-        'New Video is out',
-        'Flutter Local Notification',
-        scheduledNotificationDateTime,
-        platformChannelSpecifics,
-        payload: 'test payload');
-    await flutterLocalNotificationsPlugin.schedule(
-        1,
-        'Notification2',
-        'Flutter Local Notification2',
-        scheduledNotificationDateTime2,
-        platformChannelSpecifics,
-        payload: 'test payload');
-  }
-
   @override
   void initState() {
     super.initState();
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-    initializationSettingsiOS = IOSInitializationSettings(
-        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
-    initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsiOS);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: onSelectNotification);
+    _requestIOSPermissions();
+    _configureDidReceiveLocalNotificationSubject();
+    _configureSelectNotificationSubject();
   }
 
-  Future onDidReceiveLocalNotification(
-      int id, String title, String body, String payload) async {
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text(title),
-        content: Text(body),
-        actions: [
-          CupertinoDialogAction(
-            isDefaultAction: true,
-            child: Text('Ok'),
-            onPressed: () async {
-              Navigator.of(context, rootNavigator: true).pop();
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SecondScreen(payload),
-                ),
-              );
-            },
-          )
-        ],
-      ),
-    );
+  void _requestIOSPermissions() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
   }
 
-  Future onSelectNotification(String payload) async {
-    if (payload != null) {
-      print("payload : $payload");
-    }
-    await Navigator.push(context,
-        MaterialPageRoute(builder: (context) => SecondScreen(payload)));
+  void _configureDidReceiveLocalNotificationSubject() {
+    didReceiveLocalNotificationSubject.stream
+        .listen((ReceivedNotification receivedNotification) async {
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: receivedNotification.title != null
+              ? Text(receivedNotification.title)
+              : null,
+          content: receivedNotification.body != null
+              ? Text(receivedNotification.body)
+              : null,
+          actions: [
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text('Ok'),
+              onPressed: () async {
+                Navigator.of(context, rootNavigator: true).pop();
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondScreen(receivedNotification.payload),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      );
+    });
+  }
+
+  void _configureSelectNotificationSubject() {
+    selectNotificationSubject.stream.listen((String payload) async {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SecondScreen(payload)),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    didReceiveLocalNotificationSubject.close();
+    selectNotificationSubject.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
-        title: Text('Flutter Local Notification'),
+        title: Text('Flutter Main Page'),
       ),
       body: Center(
         child: RaisedButton(
-          onPressed: showNotification,
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => NotificationScreen()));
+          },
           child: Text(
-            'Demo',
+            'Start',
             style: Theme.of(context).textTheme.headline5,
           ),
         ),
---
+++ lib/notification_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'second_screen.dart';
+import 'main.dart';
+
+class NotificationScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  showNotification() async {
+    var scheduledNotificationDateTime =
+        DateTime.now().add(Duration(seconds: 5));
+    var scheduledNotificationDateTime2 =
+        DateTime.now().add(Duration(seconds: 10));
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'channel_ID', 'channel name', 'channel description',
+        priority: Priority.High,
+        importance: Importance.Max,
+        ticker: 'test ticker');
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.schedule(
+        0,
+        'New Video is out',
+        'Flutter Local Notification',
+        scheduledNotificationDateTime,
+        platformChannelSpecifics,
+        payload: 'test payload');
+    await flutterLocalNotificationsPlugin.schedule(
+        1,
+        'Notification2',
+        'Flutter Local Notification2',
+        scheduledNotificationDateTime2,
+        platformChannelSpecifics,
+        payload: 'test payload');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Local Notification'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: showNotification,
+          child: Text(
+            'Demo',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
+      ),
+    );
+  }
+}
